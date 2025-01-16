7ea21c9a78d72f417cd2cb5418b3b62f38d3f753
Rakshith Poojary <rakshithp7@gmail.com>
2025-01-16 16:21:46 +0530

    Implement property details page

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -119,3 +119,13 @@ export async function getProperties({ filter, query, limit }: { filter: string; 
     return [];
   }
 }
+
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await databases.getDocument(config.databaseId!, config.propertiesCollectionId!, id);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
